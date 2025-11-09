057a66e31dee6bc69cbe29ac8f95f67a203cd6f8
Naveen Kumar <naveencoke3@gmail.com>
2025-11-09 18:32:08 +0530

    Product Recommendation service initial commit

---
+++ src/main/java/com/naveen/sample/productrecommendationservice/ProductRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.sample.productrecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommendationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRecommendationServiceApplication.class, args);
+    }
+
+}
